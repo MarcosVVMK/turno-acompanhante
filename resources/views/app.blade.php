e49f03646e26f99ed86e76bb2de844962ad7cf33
Marcos Matos <marcos@coffee-code.tech>
2025-03-30 18:48:42 -0300

    feat(vite): change config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,8 +12,8 @@
 
         <!-- Scripts -->
         @routes
-        @inertiaHead
         @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
     </head>
     <body class="font-sans antialiased">
         @inertia
