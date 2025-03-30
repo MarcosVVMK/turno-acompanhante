795c2584d065bf50261cd26bdd70ddfba8038e02
Marcos Matos <marcos@coffee-code.tech>
2025-03-30 18:55:52 -0300

    feat(css)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         vue({
