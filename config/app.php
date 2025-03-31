d025bbef4356b92d0839966dc99c459e389c00de
Marcos Matos <marcos@coffee-code.tech>
2025-03-31 03:46:22 -0300

    change

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://turno-acompanhante-16af75ac5b84.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
