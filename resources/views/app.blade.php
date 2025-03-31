069cf808f819464bef2eef3f592d547cbe415191
Marcos Matos <marcos@coffee-code.tech>
2025-03-30 22:08:39 -0300

    feat:vite order
    (cherry picked from commit e6aa1aab3065350b51b694fb6f27121ec46745e7)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @routes
         @inertiaHead
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
